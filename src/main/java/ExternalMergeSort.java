e1440476521a18df79161c196c11978b5ac90c6e
Victor Khovanskiy <victor.khovanskiy@gmail.com>
2017-01-22 17:41:50 +0300

    Dump

--- src/main/java/ExternalMergeSort.java
+++ src/main/java/ExternalMergeSort.java
@@ -1,14 +1,7 @@
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author Victor Khovanskiy
@@ -17,17 +10,16 @@ import java.util.PriorityQueue;
 public class ExternalMergeSort implements Closeable {
     private final RandomAccessFile input;
     private final RandomAccessFile output;
-    private final int blockSize;
-    private final long totalSize;
+    private final int M;
+    private final long T;
+    private final File catalog = new File("catalog/");
 
-    public ExternalMergeSort(File input, File output, int blockSize) throws IOException {
-        this.totalSize = input.length();
-        if ((long) blockSize * blockSize < totalSize) {
-            throw new IllegalArgumentException(String.format("Block's size must be greater or equal than block's number: %1$d * %1$d < %2$d (min %3$d)", blockSize, totalSize, (int) Math.sqrt(totalSize)));
-        }
+    public ExternalMergeSort(File input, File output, int M) throws IOException {
+        catalog.mkdirs();
+        this.T = input.length();
         this.input = new RandomAccessFile(input, "rw");
         this.output = new RandomAccessFile(output, "rw");
-        this.blockSize = blockSize;
+        this.M = M;
     }
 
     @Override
@@ -39,54 +31,154 @@ public class ExternalMergeSort implements Closeable {
         }
     }
 
-    public void execute() throws IOException {
-        byte[] block = new byte[blockSize];
+    private int fileId;
+    private Deque<File> files = new ArrayDeque<>();
+
+    public void splitPhase() throws IOException {
+        byte[] buffer = new byte[M];
+        try (FileInputStream inputStream = new FileInputStream("input")) {
+            int bufferSize;
+            while ((bufferSize = readBuffer(inputStream, buffer)) > 0) {
+                Arrays.sort(buffer);
+                File file = new File(catalog, "output_" + fileId);
+                files.add(file);
+                try (FileOutputStream outputStream = new FileOutputStream(file, true)) {
+                    outputStream.write(buffer, 0, bufferSize);
+                }
+                ++fileId;
+            }
+        }
+    }
+
+    public void mergePhase() throws IOException {
+
+    }
+
+    public int readBuffer(FileInputStream inputStream, byte[] buffer) throws IOException {
+        int bufferOffset = 0;
+        do {
+            int readCount = inputStream.read(buffer, bufferOffset, buffer.length - bufferOffset);
+            if (readCount < 0) {
+                break;
+            }
+            bufferOffset += readCount;
+        } while (bufferOffset < buffer.length);
+        return bufferOffset;
+    }
+
+    public int phase1() throws IOException {
+        byte[] block = new byte[M];
         int length;
         int blockNumber = 0;
         do {
-            length = readBlock(block, 0, blockSize);
+            length = readBlock(input, block, 0, M);
             if (length == 0) {
                 continue;
             }
             Arrays.sort(block);
-            //log.info(Arrays.toString(block) + " " + length);
-            input.seek(blockNumber * blockSize);
-            writeBlock(block, length);
+            debug(Arrays.toString(block));
+            input.seek(blockNumber * M);
+            input.write(block, 0, length);
             ++blockNumber;
-        } while (length == blockSize);
-        input.seek(0);
-        PriorityQueue<Pointer<Byte>> heap = new PriorityQueue<>();
+        } while (length == M);
+        return blockNumber;
+    }
 
-        int bufferSize = blockSize / blockNumber;
-        for (int i = 0; i < blockNumber; ++i) {
-            int available;
-            if (i < blockNumber - 1) {
-                available = blockSize;
+    public void phase2() throws IOException {
+        final int k = 16;
+
+        int bufferSize = M;
+        // (k + 1) * L <= M
+
+        RandomAccessFile inputAtIteration = input;
+        RandomAccessFile outputAtIteration = output;
+        long blockSizeAtIteration = M;
+
+        Deque<byte[]> buffersCache = new ArrayDeque<>();
+        byte[] outputBuffer = new byte[bufferSize];
+        while (blockSizeAtIteration < T) {
+            debug("Новая итерация с blockSizeAtIteration = " + blockSizeAtIteration, true);
+            inputAtIteration.seek(0);
+            outputAtIteration.seek(0);
+            long fileOffset = 0;
+            int blocksCountAtIteration = getBlocksNumber(T, blockSizeAtIteration);
+            for (int blockMerged = 0; blockMerged < blocksCountAtIteration; blockMerged += k) {
+                PriorityQueue<Pointer> heap = new PriorityQueue<>();
+                for (int i = 0; i < Math.min(k, blocksCountAtIteration - blockMerged); ++i) {
+                    long available = Math.min(T - fileOffset, blockSizeAtIteration);
+
+                    byte[] nodeBuffer;
+                    if (buffersCache.isEmpty()) {
+                        nodeBuffer = new byte[bufferSize];
+                    } else {
+                        nodeBuffer = buffersCache.pop();
+                    }
+                    heap.add(new Pointer(inputAtIteration, i, nodeBuffer, fileOffset, available));
+                    fileOffset += blockSizeAtIteration;
+                }
+
+                int bufferIndex = 0;
+                while (!heap.isEmpty()) {
+                    Pointer pointer = heap.poll();
+                    debug("Извлекаем " + pointer);
+                    byte element = pointer.next();
+                    debug("Пишем в буфер " + element);
+                    outputBuffer[bufferIndex] = element;
+                    ++bufferIndex;
+                    // Если буфер для записи заполнен, то пишем его на диск и обнуляем смещение.
+                    if (bufferIndex == outputBuffer.length) {
+                        debug("Пишем буфер в файл");
+                        outputAtIteration.write(outputBuffer, 0, outputBuffer.length);
+                        bufferIndex = 0;
+                    }
+                    // Если узел содержит элементы, то добавляет его обратно в кучу.
+                    if (pointer.hasNext()) {
+                        heap.add(pointer);
+                    } else {
+                        buffersCache.push(pointer.buffer);
+                    }
+                }
+                if (bufferIndex > 0) {
+                    outputAtIteration.write(outputBuffer, 0, bufferIndex);
+                }
+            }
+            if (inputAtIteration == output) {
+                inputAtIteration = input;
+                outputAtIteration = output;
             } else {
-                available = (int) (totalSize - (blockNumber - 1) * blockSize);
+                inputAtIteration = output;
+                outputAtIteration = input;
             }
-            heap.add(new Pointer<>(i, new Byte[bufferSize], available, this::byte2byte));
+            blockSizeAtIteration <<= 1;
         }
 
-        int blockIndex = 0;
-        while (!heap.isEmpty()) {
-            Pointer<Byte> pointer = heap.poll();
-            //System.out.println(pointer);
-            byte element = pointer.next();
-            //System.out.println("Put " + element);
-            block[blockIndex] = element;
-            ++blockIndex;
-            if (blockIndex == block.length) {
-                output.write(block, 0, block.length);
-                blockIndex = 0;
-            }
-            if (pointer.hasNext()) {
-                heap.add(pointer);
+        if (outputAtIteration != input) {
+            input.seek(0);
+            output.seek(0);
+            byte[] buffer = new byte[M];
+            int read;
+            while ((read = readBlock(input, buffer, 0, buffer.length)) > 0) {
+                output.write(buffer, 0, read);
             }
         }
-        if (blockIndex > 0) {
-            output.write(block, 0, blockIndex);
-        }
+    }
+
+    public void debug(Object object) {
+        //System.out.println(object);
+    }
+
+    public void debug(Object object, boolean shouldWrite) {
+        System.out.println(object);
+    }
+
+    public static int getBlocksNumber(long totalSize, long blockSize) {
+        return (int) Math.ceil(1d * totalSize / blockSize);
+    }
+
+    public void execute() throws IOException {
+        //phase1();
+        //phase2();
+        splitPhase();
     }
 
     protected void byte2byte(byte[] buf, Byte[] temp, int length) {
@@ -100,81 +192,100 @@ public class ExternalMergeSort implements Closeable {
         void accept(A a, B b, int length);
     }
 
-    @ToString
-    protected class Pointer<T extends Comparable<T>> implements Comparable<Pointer<T>>, Iterator<T> {
+    protected class Pointer implements Comparable<Pointer>, Iterator<Byte> {
         private final int number;
-        private final T[] buffer;
+        private final byte[] buffer;
 
+        /**
+         * Позиция текущего элемента в буфере
+         */
         private int bufferIndex;
+        /**
+         * Доступный размер буфера
+         */
         private int bufferAvailable;
 
-        private int blockIndex;
-        private final int blockAvailable;
+        /**
+         * Смещение блока относительно начала файла
+         */
+        private final long fileOffset;
+        /**
+         * Смещение относительно начала блока
+         */
+        private long blockOffset;
+        /**
+         * Размер блока
+         */
+        private final long blockAvailable;
 
-        private final byte[] temp;
-        private final Converter<byte[], T[]> converter;
+        private final RandomAccessFile file;
 
-        public Pointer(int number, T[] buffer, int blockAvailable, Converter<byte[], T[]> converter) {
-            this.number = number;
+        public Pointer(RandomAccessFile file, int ordinal, byte[] buffer, long fileOffset, long blockAvailable) {
+            if (fileOffset + blockAvailable > T) {
+                throw new AssertionError();
+            }
+            this.file = file;
+            this.number = ordinal;
             this.buffer = buffer;
-            this.temp = new byte[buffer.length];
+            this.fileOffset = fileOffset;
             this.blockAvailable = blockAvailable;
-            this.converter = converter;
         }
 
         @Override
-        public int compareTo(Pointer<T> another) {
+        public int compareTo(Pointer another) {
             fetch();
             another.fetch();
-            assert another.buffer[another.bufferIndex] != null;
-            return this.buffer[bufferIndex].compareTo(another.buffer[another.bufferIndex]);
+            return Byte.compare(this.buffer[bufferIndex], another.buffer[another.bufferIndex]);
         }
 
         @Override
         public boolean hasNext() {
-            return blockIndex < blockAvailable;
+            return blockOffset < blockAvailable;
         }
 
         @Override
-        public T next() {
+        public Byte next() {
             fetch();
             ++bufferIndex;
+            ++blockOffset;
             return buffer[bufferIndex - 1];
         }
 
         protected void fetch() {
-            if (bufferIndex == bufferAvailable && blockIndex < blockAvailable) {
+            if (bufferIndex == bufferAvailable && blockOffset < blockAvailable) {
                 int read;
                 try {
-                    input.seek(number * blockSize + blockIndex);
-                    read = readBlock(temp, 0, buffer.length);
+                    file.seek(fileOffset + blockOffset);
+                    read = readBlock(file, buffer, 0, buffer.length);
                 } catch (IOException e) {
                     throw new NoSuchElementException(e.getMessage());
                 }
                 if (read == 0) {
                     throw new NoSuchElementException();
                 }
+                // Сдвигаем смещение относительно начала блока на количество прочитанных элементов буфера
+                //blockOffset += bufferAvailable;
+                // Доступный размер буфера равен количеству прочитанных элементов
                 bufferAvailable = read;
-                blockIndex += read;
-                converter.accept(temp, buffer, read);
+                // Обнуляем позицию в буфере
                 bufferIndex = 0;
             }
         }
+
+        public String toString() {
+            return "Pointer(number=" + this.number + ", buffer=" + Arrays.toString(this.buffer) + ", bufferIndex=" + this.bufferIndex + ", bufferAvailable=" + this.bufferAvailable + ", fileOffset=" + this.fileOffset + ", blockOffset=" + this.blockOffset + ", blockAvailable=" + this.blockAvailable + ")";
+        }
     }
 
-    protected int readBlock(byte[] block, int offset, int length) throws IOException {
+    protected int readBlock(RandomAccessFile file, byte[] block, int offset, int length) throws IOException {
         int n = 0;
         do {
-            int count = input.read(block, offset + n, length - n);
+            int count = file.read(block, offset + n, length - n);
             if (count < 0) {
                 return n;
             }
             n += count;
         } while (n < length);
         return n;
-    }
-
-    protected void writeBlock(byte[] block, int length) throws IOException {
-        input.write(block, 0, length);
     }
 }
--- src/test/java/ExternalMergeSortTest.java
+++ src/test/java/ExternalMergeSortTest.java
@@ -1,13 +1,13 @@
 import lombok.extern.slf4j.Slf4j;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -18,74 +18,143 @@ public class ExternalMergeSortTest {
 
     private static final String INPUT_FILENAME = "input";
     private static final String OUTPUT_FILENAME = "output";
-    private static final int COUNT_OF_NUMBERS = 1024 * 1024;
-    private static final int BLOCK_SIZE = 1024;
     private static final int SEED = 12345;
     private static final Random RANDOM = new Random(SEED);
-    private byte[] array = new byte[COUNT_OF_NUMBERS];
 
-    @Before
-    public void before() throws IOException {
-        log.info("Prepare array");
-        RANDOM.nextBytes(array);
-
-        log.info("Prepare data file");
-        delete(INPUT_FILENAME);
-        delete(OUTPUT_FILENAME);
-        try (RandomAccessFile file = new RandomAccessFile(new File(INPUT_FILENAME), "rw")) {
-            for (int i = 0; i < array.length; ++i) {
-                file.writeByte(array[i]);
-            }
-        }
-    }
-
-    @Test
+    /*@Test
     public void internal() {
         log.info("Starting internal sorting...");
         long start = System.currentTimeMillis();
         Arrays.sort(array);
         long duration = System.currentTimeMillis() - start;
         log.info("Done in " + (duration / 1000f) + " s");
+    }*/
+
+    @Test
+    public void external() {
+        List<ExecutionTest> resultList = new ArrayList<>();
+        for (int T : Arrays.asList(100000, 1000000, 10000000)) {
+            int minM = (int) Math.sqrt(T) + 1;
+            for (int M = minM; M < T; M *= 10) {
+                ExecutionTest test = new ExecutionTest(T, M);
+                test.run();
+                resultList.add(test);
+            }
+        }
+        for (int i = 0; i < resultList.size(); ++i) {
+            ExecutionTest result = resultList.get(i);
+            System.out.println(String.format("%d\t%d\t%d\t%d\t%d", i + 1, result.T, result.M, result.externalTime, result.internalTime));
+        }
     }
 
     @Test
-    public void external() throws IOException {
-        log.info("Starting external sorting...");
-
-        long start = System.currentTimeMillis();
-        try (ExternalMergeSort mergeSort = new ExternalMergeSort(new File(INPUT_FILENAME), new File(OUTPUT_FILENAME), BLOCK_SIZE)) {
-            mergeSort.execute();
-        }
-        long duration = System.currentTimeMillis() - start;
-        log.info("Done in " + (duration / 1000f) + " s");
-
-        log.info("Starting testing the result...");
-        byte[] block = new byte[BLOCK_SIZE];
-        int previous = Byte.MIN_VALUE - 1;
-
-        start = System.currentTimeMillis();
-        try (RandomAccessFile file = new RandomAccessFile(OUTPUT_FILENAME, "r")) {
-            int read;
-            while ((read = file.read(block, 0, block.length)) >= 0) {
-                for (int i = 0; i < read; ++i) {
-                    byte current = block[i];
-                    Assert.assertTrue("Previous byte must be less or equal than current", previous <= current);
-                    previous = current;
-                }
-            }
-        }
-        duration = System.currentTimeMillis() - start;
-        log.info("Done in " + (duration / 1000f) + " s");
-    }
-
-    @After
-    public void after() throws IOException {
-        log.info("Clean up files");
-        delete(INPUT_FILENAME);
-        delete(OUTPUT_FILENAME);
+    public void single() {
+        ExecutionTest result = new ExecutionTest(100000, 31700);
+        result.run();
+        System.out.println(String.format("%d\t%d\t%d", result.T, result.M, result.externalTime));
     }
 
     private boolean delete(String filename) {
         return new File(filename).delete();
+    }
+
+    public class ExecutionTest implements Runnable {
+        private final int T;
+        private final int M;
+        private long externalTime;
+        private long internalTime;
+
+        private final byte[] array;
+
+        public ExecutionTest(int T, int M) {
+            this.T = T;
+            this.M = M;
+            this.array = new byte[T];
+        }
+
+        public void before() throws IOException {
+            log.info("Prepare array");
+            /*for (int i = array.length - 1; i >= 0; --i) {
+                array[i] = (byte) i;
+            }*/
+            RANDOM.nextBytes(array);
+
+            log.info("Prepare data file");
+            if (delete(INPUT_FILENAME)) {
+                log.info("INPUT is deleted");
+            }
+            if (delete(OUTPUT_FILENAME)) {
+                log.info("OUTPUT is deleted");
+            }
+            try (RandomAccessFile file = new RandomAccessFile(new File(INPUT_FILENAME), "rw")) {
+                int written = 0;
+                while (written < array.length) {
+                    int length = Math.min(M, array.length - written);
+                    file.write(array, written, length);
+                    written += length;
+                }
+            }
+        }
+
+        public void internal() {
+            log.info(String.format("Starting internal sorting T = %d, M = %d", T, M));
+
+            long start = System.currentTimeMillis();
+            Arrays.sort(array);
+            long duration = System.currentTimeMillis() - start;
+            this.internalTime = duration;
+            log.info("Done in " + (duration / 1000f) + " s");
+        }
+
+        public void external() throws IOException {
+            log.info(String.format("Starting external sorting T = %d, M = %d", T, M));
+
+            long start = System.currentTimeMillis();
+            try (ExternalMergeSort mergeSort = new ExternalMergeSort(new File(INPUT_FILENAME), new File(OUTPUT_FILENAME), M)) {
+                mergeSort.execute();
+            }
+            long duration = System.currentTimeMillis() - start;
+            this.externalTime = duration;
+            log.info("Done in " + (duration / 1000f) + " s");
+
+            log.info("Starting testing the result...");
+            byte[] block = new byte[M];
+            int previous = Byte.MIN_VALUE - 1;
+            start = System.currentTimeMillis();
+            try (RandomAccessFile file = new RandomAccessFile(OUTPUT_FILENAME, "r")) {
+                int read;
+                while ((read = file.read(block, 0, block.length)) >= 0) {
+                    for (int i = 0; i < read; ++i) {
+                        byte current = block[i];
+                        Assert.assertTrue("Previous byte must be less or equal than current", previous <= current);
+                        previous = current;
+                    }
+                }
+            }
+            duration = System.currentTimeMillis() - start;
+            log.info("Done in " + (duration / 1000f) + " s");
+        }
+
+        public void after() throws IOException {
+            log.info("Clean up files");
+            if (delete(INPUT_FILENAME)) {
+                log.info("INPUT is deleted");
+            }
+            if (delete(OUTPUT_FILENAME)) {
+                log.info("OUTPUT is deleted");
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                before();
+                external();
+                internal();
+                after();
+            } catch (Exception e) {
+                log.error(e.getLocalizedMessage(), e);
+            }
+        }
     }
 }
